518ef33cda175a17cfa68563096339768b1583c9
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-13 04:18:50 +0600

    toogle with google signin signout with firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 
 import './App.css';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import app from './firebase/firebase.init';
-const auth = getAuth(app)
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth';
+import app from './firebase/firebase.init'
+import { useState } from 'react';
+const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
-  const handalGoogleSignIn = () =>{
-    console.log('click');
+  const [user, setUser] = useState({});
+ const provider = new GoogleAuthProvider();
+  const handalSignIn = () =>{
+    signInWithPopup(auth, provider)
+    .then(result =>{
+      const user = result.user;
+      setUser(user)
+      console.log(user);
+    })
+    .catch(error =>{
+      console.error('error', error);
+    })
+  };
+  const handalSignOut = ()=>{
+    signOut(auth)
+    .then(()=>{
+      setUser({});
+    })
+    .catch(()=>{
+      setUser({});
+    })
   }
   return (
     <div className="App">
-      <button onClick={handalGoogleSignIn}>Google Sign In</button>
+      {/* Conditional REndering  */}
+
+      { user.email ? 
+        <button onClick={handalSignOut}>Sign Out</button> :
+        <button onClick={handalSignIn}>Sign In With Google</button>
+      }
+      {  user.email && <div>
+        <h2>UserName: {user.displayName}</h2>
+        <p>Email Address: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
