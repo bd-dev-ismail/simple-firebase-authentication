b6666b057316694b46c23f09042250adf46a10b5
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-13 00:33:35 +0600

    frist project with firebase

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQlB4vRDb-1z2zhqb2HOwaQgu2sRs7a-I",
+  authDomain: "simple-firebase-authenti-8dfa8.firebaseapp.com",
+  projectId: "simple-firebase-authenti-8dfa8",
+  storageBucket: "simple-firebase-authenti-8dfa8.appspot.com",
+  messagingSenderId: "231689482008",
+  appId: "1:231689482008:web:9840232ba940c92581386d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
